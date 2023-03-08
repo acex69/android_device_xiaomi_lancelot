248b6e3dff4da3426aa8205795b30ebcc6455071
Đł Ɽ₳łⱫɆⱠ ㋡ <122148266+acex69@users.noreply.github.com>
2023-03-08 18:39:49 +0530

    Update aosp_lancelot.mk

--- aosp_lancelot.mk
+++ aosp_lancelot.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-
-
-
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
